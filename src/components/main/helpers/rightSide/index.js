b919cd2f7e68e0f9d10bbe3b74c3e29271f03485
Luis Trujillo <lmtrujillo83695@umanizales.edu.co>
2023-05-22 03:04:40 -0500

    Se hace el llamdo de los elementos hijos en el right side

--- src/components/main/helpers/rightSide/index.js
+++ src/components/main/helpers/rightSide/index.js
@@ -1,11 +1,23 @@
 //Se importa libreria de react
 import React from 'react';
-
+//Se importa elemnto de experincia laboral
+import { WorkExperience } from './helpers/workExperience';
+import { Flp, Uno27 } from './helpers/workExperience/companies';
+//Se importa elemento de estudios
+import { Studies } from './helpers/studies';
+import { UM, Latino, SantaJuana } from './helpers/studies/institutions';
+//Se importa elemento de idiomas
+import { Languages } from './helpers/languages';
+// Se importa elemento de habilidades
+import { Skills } from './helpers/skills';
 //Se genera componente
 export const RightSide = () => {
     return (
         <div className='main__right-side'>
-            Eso es el contenido del lateral derecho
+            <WorkExperience experiencia1={Flp} experiencia2={Uno27} />
+            <Studies institution1={SantaJuana} institution2={Latino} institution3={UM} />
+            <Languages/>
+            <Skills/>
         </div>
     );
 };
