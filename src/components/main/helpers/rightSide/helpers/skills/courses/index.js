fa6d4d7e58e90b9f6a5d5ca1cddf05c9cd368580
Luis Trujillo <lmtrujillo83695@umanizales.edu.co>
2023-05-22 04:40:08 -0500

    Se soluciona problema de desconfiguracion regresndo un commit

---
+++ src/components/main/helpers/rightSide/helpers/skills/courses/index.js
@@ -0,0 +1,13 @@
+export const CloudComputing = {
+    nameCourse: 'Cloud Computing',
+    descriptionCourse: 'Descubre el mundo del Cloud Computing de la mano de EOI (Escuela de Organización Industrial)',
+    durationCourse: '40 horas',
+    approvedCourse: 'True'
+};
+
+export const Methodology ={
+    nameCourse: 'Metodología de la programación de sistemas informáticos',
+    descriptionCourse: 'Enfoque estructurado y sistemático para el desarrollo de software y sistemas informáticos',
+    durationCourse: '40 horas',
+    approvedCourse: 'True'
+};
