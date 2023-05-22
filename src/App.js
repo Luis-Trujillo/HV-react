676f468cdd9ca48cd27f1c0be15dd3076bcb6bc1
Luis Trujillo <lmtrujillo83695@umanizales.edu.co>
2023-05-22 00:36:46 -0500

    Se modifica el app principal para algunos llamados

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import './App.css';
 //Se importan componetes
 import { Header } from './components/header';
+import { Main } from './components/main';
 //Se importan estilos
 import './components/header/styles/header.css';
+import './components/main/styles/main.css';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Main/>
     </div>
   );
 }
