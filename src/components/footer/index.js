c7c759c69affeeec4f9d4387c1b3931a46731c5a
Luis Trujillo <lmtrujillo83695@umanizales.edu.co>
2023-05-25 16:11:09 -0500

    Se crea componente de footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,10 @@
+//Se importa libreria de react
+import React from 'react'
+
+
+//Se genera componente de footer de la página
+export const Footer = () => {
+    return (
+        <div>Footer</div>
+    );
+};
